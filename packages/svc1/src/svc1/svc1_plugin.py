91858f5d949c750e0371edba8fa6439c3a7639a0
Shawn Oster <shawn.oster@gmail.com>
2025-04-08 22:11:30 -0700

    feat(core): implement Svc1Plugin with collect and evaluate methods

---
+++ packages/svc1/src/svc1/svc1_plugin.py
@@ -0,0 +1,32 @@
+
+from core.core_api import PluginBase
+
+
+class Svc1Plugin(PluginBase):
+    """
+    Plugin for svc1.
+    """
+
+    def collect(self, data: dict) -> str:
+        """
+        Process the input dictionary and return a modified dictionary.
+
+        Args:
+            data (Dict): Input data to be processed.
+
+        Returns:
+            Dict: Processed data.
+        """
+        return "svc1"
+
+    def evaluate(self, data: dict) -> dict:
+        """
+        Evaluate the input dictionary and return a modified dictionary.
+
+        Args:
+            data (Dict): Input data to be evaluated.
+
+        Returns:
+            Dict: Evaluated data.
+        """
+        return {"svc1": "svc1"}
