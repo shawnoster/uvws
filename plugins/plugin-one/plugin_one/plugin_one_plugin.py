0118dd9584895f6dda7b93ed3fb0c5080fb4dd94
Shawn Oster <shawn.oster@guild.com>
2025-04-13 17:12:51 -0700

    fix(plugin-one): update evaluate method to return correct serviceName

--- plugins/plugin-one/plugin_one/plugin_one_plugin.py
+++ plugins/plugin-one/plugin_one/plugin_one_plugin.py
@@ -31,4 +31,4 @@ class PluginOnePlugin(PluginBase):
         Returns:
             Dict: Evaluated data.
         """
-        return {"serviceName": "test"}
+        return {"serviceName": "test-two"}
